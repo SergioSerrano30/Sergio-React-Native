31276f1c9d9f2a98df3e54808ecc6913d074aa66
SergioSerrano30 <84033208+SergioSerrano30@users.noreply.github.com>
2022-09-28 15:23:22 -0600

    4.6 Finalizado

--- app-test-1/src/App.tsx
+++ app-test-1/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 //import { TiposBasicos } from './typescript/TiposBasicos';
-import { ObjetosLiterales } from './typescript/ObjetosLiterales';
+// import { ObjetosLiterales } from './typescript/ObjetosLiterales';
+import { Contador } from './components/Contador';
 
 const App = () =>{
   return (
@@ -8,7 +9,8 @@ const App = () =>{
       <h1>Introducción a Type Script con React</h1>
       <hr />
       {/* <TiposBasicos/> */}
-      <ObjetosLiterales/>
+      {/* <ObjetosLiterales/> */}
+      {<Contador/>}
     </div>
   )
 }
